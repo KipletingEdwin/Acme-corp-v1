72adb576d4f245f67d6ff23e764d581e147be349
KipletingEdwin <kipletingedwin4@gmail.com>
2026-07-26 10:45:16 +0100

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -48,6 +48,7 @@ export const Login = () => {
         <div className="space-y-2">
             <div className="flex items-center justify-between">
                 <label htmlFor="password" >Password</label>
+                <Link to="/forgot-password" className="text-xs text-primary hover:underline">Forgot Password</Link>
 
             </div>
         </div>
